b456547ee8427595fbdb5494cab5c91d62214e8d
tlugaro <77947147+tlugaro@users.noreply.github.com>
2024-03-28 21:42:10 -0300

    Subida
    Hackaton

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,22 +1,73 @@
 import argparse
-
+import librosa
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import csv
 def load_data(file_path):
-    # TODO: Load data from CSV file
+    df = pd.read_csv(file_path)
 
     return df
 
 def clean_data(df):
-    # TODO: Handle missing values, outliers, etc.
+    df_clean=df.dropna()
 
     return df_clean
+def preprocess_test_data(df):
+    X_paths = df['Path'].tolist()
 
+    idx = df['Idx'].tolist()
+    num_classes = 6
+
+    def preprocess_audio(file_path):
+        y, sr = librosa.load(file_path)
+        mel_spec = librosa.feature.melspectrogram(y=y, sr=sr)
+        mel_spec = librosa.power_to_db(mel_spec, ref=np.max)
+        return mel_spec
+
+    X_processed = [preprocess_audio("../"+file_path) for file_path in X_paths]
+    X_processed = np.array(X_processed)
+    X_processed_expanded = np.expand_dims(X_processed, axis=-1)
+    scaler = StandardScaler()
+    X_reshaped = X_processed_expanded.reshape((X_processed_expanded.shape[0], -1))
+    X_scaled = scaler.fit_transform(X_reshaped)
+    X_scaled = X_scaled.reshape((X_processed_expanded.shape))
+    df_processed = pd.DataFrame()
+    df_processed["x"] = [X_scaled[i] for i in range(len(X_scaled))] 
+    df_processed["idx"] = idx
+    return df_processed
 def preprocess_data(df):
-    # TODO: Generate new features, transform existing features, resampling, etc.
+    
+    X_paths = df['Path'].tolist()
+
+    y_labels = df['Label'].tolist()
+    num_classes = 6
+
+    def preprocess_audio(file_path):
+        y, sr = librosa.load(file_path)
+        mel_spec = librosa.feature.melspectrogram(y=y, sr=sr)
+        mel_spec = librosa.power_to_db(mel_spec, ref=np.max)
+        return mel_spec
+
+    X_processed = [preprocess_audio("../"+file_path) for file_path in X_paths]
+    X_processed = np.array(X_processed)
+    y = np.array(y_labels)
+
+    X_processed_expanded = np.expand_dims(X_processed, axis=-1)
+    scaler = StandardScaler()
+
+    X_reshaped = X_processed_expanded.reshape((700, -1))
+    X_scaled = scaler.fit_transform(X_reshaped)
+    X_scaled = X_scaled.reshape((X_processed_expanded.shape))
+
+    df_processed = pd.DataFrame()
+    df_processed["x"] = [X_scaled[i] for i in range(len(X_scaled))] 
+    df_processed["y"] = y
 
     return df_processed
 
 def save_data(df, output_file):
-    # TODO: Save processed data to a CSV file
+    df.to_pickle(output_file)
     pass
 
 def parse_arguments():
@@ -30,17 +81,33 @@ def parse_arguments():
     parser.add_argument(
         '--output_file', 
         type=str, 
-        default='data/processed_data/', 
+        default='data/processed_data/data.pkl',
+        help='Folder path to save the processed data'
+    )
+    parser.add_argument(
+        '--test_input_file',
+        type=str,
+        default='data/paths_test.csv',
+        help='Path to the raw data file to process'
+    )
+    parser.add_argument(
+        '--test_output_file', 
+        type=str, 
+        default='data/processed_data/test_data.pkl',
         help='Folder path to save the processed data'
     )
     return parser.parse_args()
 
-def main(input_file, output_file):
+def main(input_file, output_file,test_input_file,test_output_file):
     df = load_data(input_file)
     df_clean = clean_data(df)
     df_processed = preprocess_data(df_clean)
     save_data(df_processed, output_file)
+    test_df = load_data(test_input_file)
+    test_df_clean = clean_data(test_df)
+    test_df_processed = preprocess_test_data(test_df_clean)
+    save_data(test_df_processed, test_output_file)
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.input_file, args.output_file)
+    main(args.input_file, args.output_file, args.test_input_file,args.test_output_file)
--- src/model_training.py
+++ src/model_training.py
@@ -1,20 +1,50 @@
 import pandas as pd
 import argparse
+import numpy as np
+import joblib
+
+from tensorflow.keras import layers, models
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 def load_data(file_path):
-    # TODO: Load processed data from CSV file
+    df = pd.read_pickle(file_path)
     return df
 
 def split_data(df):
-    # TODO: Split data into training and validation sets (the test set is already provided in data/test_data.csv)
+    print(df)
+    X_train, X_val, y_train, y_val = train_test_split(np.array(df["x"].tolist()), np.array(df["y"].tolist()), test_size=0.1, random_state=24)
+
     return X_train, X_val, y_train, y_val
 
-def train_model(X_train, y_train):
-    # TODO: Initialize your model and train it
+def train_model(X_train,X_val,y_train, y_val):
+    np.random.seed(21)
+    tf.random.set_seed(21)
+    num_clases=len(set(y_train))
+    model = models.Sequential([
+        layers.Input(shape=(128, 216)),
+        layers.Conv1D(32, 3, activation='relu'),
+        layers.MaxPooling1D(2),
+        layers.Conv1D(64, 3, activation='relu'),
+        layers.MaxPooling1D(2),
+        layers.LSTM(128, return_sequences=True),
+        layers.Dropout(0.15),
+        layers.LSTM(64),
+        layers.Dropout(0.15),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(num_clases, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(X_train, y_train, epochs=50, batch_size=16, validation_data=(X_val, y_val))
+
     return model
 
-def save_model(model, model_path):
-    # TODO: Save your trained model
+def save_model(model, model_path,model_file2):
+    model.save(model_path)
+    joblib.dump(model,model_file2)
     pass
 
 def parse_arguments():
@@ -22,23 +52,29 @@ def parse_arguments():
     parser.add_argument(
         '--input_file', 
         type=str, 
-        default='data/processed_data/', 
+        default='data/processed_data/data.pkl',
         help='Path to the processed data file to train the model'
     )
     parser.add_argument(
         '--model_file', 
         type=str, 
-        default='models/model.pkl', 
+        default='models/model.h5',
+        help='Path to save the trained model'
+    )
+    parser.add_argument(
+        '--model_file2',
+        type=str,
+        default='models/model.pkl',
         help='Path to save the trained model'
     )
     return parser.parse_args()
 
-def main(input_file, model_file):
+def main(input_file, model_file,model_file2):
     df = load_data(input_file)
     X_train, X_val, y_train, y_val = split_data(df)
-    model = train_model(X_train, y_train)
-    save_model(model, model_file)
+    model = train_model(X_train, X_val, y_train, y_val)
+    save_model(model, model_file,model_file2)
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.input_file, args.model_file)
+    main(args.input_file, args.model_file, args.model_file2)
